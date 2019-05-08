fc2a1a427aa8a455d8a332548a1dfd45d541b069
jonathan_scott <jonathan_scott@icloud.com>
2019-05-08 07:03:58 +0200

    Implementation based on number of iterations. Three unit tests.

--- CalculatePi/CalculatePi.Test/TestGregory-Leibniz.cs
+++ CalculatePi/CalculatePi.Test/TestGregory-Leibniz.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using CalculatePi.Pi;
+using System;
 
 namespace Tests
 {
@@ -11,12 +12,14 @@ namespace Tests
         }
 
         [Test]
-        public void Test1()
+        [TestCase(1000, 0.0005)]
+        [TestCase(10000, 0.00006)]
+        [TestCase(1000000, 0.000001)]
+        public void Test(int iterations, double expectedDifference)
         {
             var pi = new Gregory_Leibniz();
-            pi.Calculate();
-            Assert.AreEqual(3.1, pi.Calculate());
-            Assert.Pass();
+            pi.Iterations = iterations;
+            Assert.That(Math.Abs(3.14159265359 - pi.Calculate()), Is.LessThan(expectedDifference));
         }
     }
 }
--- CalculatePi/CalculatePi/Gregory-Leibniz.cs
+++ CalculatePi/CalculatePi/Gregory-Leibniz.cs
@@ -5,6 +5,10 @@ namespace CalculatePi
     {
         public class Gregory_Leibniz
         {
+            #region
+            public int Iterations { get; set; }
+            #endregion
+
             public Gregory_Leibniz()
             {
 
@@ -12,7 +16,12 @@ namespace CalculatePi
 
             public double Calculate()
             {
-                return (4/1)-(4/3)+(4/5)-(4/7)+(4/9);
+                double result = 0.0;
+                for (int i = 0; i < Iterations; i++)
+                {
+                    result += ((4.0 / (1.0 + i * 4.0)) - (4.0 / (3.0 + i * 4.0)));
+                }
+                return result;
             }
         }
     }
