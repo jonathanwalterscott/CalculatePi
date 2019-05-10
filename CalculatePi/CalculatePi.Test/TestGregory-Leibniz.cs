7e383b13ee0ba466a89654e732a7be74b10113a2
jonathan_scott <jonathan_scott@icloud.com>
2019-05-10 03:01:03 +0200

    Allow for the parallelization of the calculation by specifying start iteration and number of iterations. It is left to the caller to determine these values as a part of that parallelization.

--- CalculatePi/CalculatePi.Test/TestGregory-Leibniz.cs
+++ CalculatePi/CalculatePi.Test/TestGregory-Leibniz.cs
@@ -15,11 +15,22 @@ namespace Tests
         [TestCase(1000, 0.0005)]
         [TestCase(10000, 0.00006)]
         [TestCase(1000000, 0.000001)]
-        public void Test(int iterations, double expectedDifference)
+        public void Test_NumberOfIterations(int iterations, double expectedDifference)
         {
             var pi = new Gregory_Leibniz();
-            pi.Iterations = iterations;
+            pi.NumberOfIterations = iterations;
             Assert.That(Math.Abs(3.14159265359 - pi.Calculate()), Is.LessThan(expectedDifference));
+        }
+
+        [Test]
+        [TestCase(0, 1000, 3.1410926)]
+        [TestCase(0, 1000000, 3.14159265359)]
+        [TestCase(500, 1000, 0.0004999)]
+        public void Test_Iterations(int startIteration, int numberOfIterations, double expected)
+        {
+            var pi = new Gregory_Leibniz();
+            pi.Iterations(startIteration, numberOfIterations);
+            Assert.That(Math.Abs(expected - pi.Calculate()), Is.LessThan(0.000001));
         }
     }
 }
--- CalculatePi/CalculatePi/Gregory-Leibniz.cs
+++ CalculatePi/CalculatePi/Gregory-Leibniz.cs
@@ -6,7 +6,13 @@ namespace CalculatePi
         public class Gregory_Leibniz
         {
             #region
-            public int Iterations { get; set; }
+            public int NumberOfIterations { get; set; }
+            public int StartIteration { get; set; }
+            public void Iterations(int start, int numberOfIterations)
+            {
+                StartIteration = start;
+                NumberOfIterations = numberOfIterations;
+            }
             #endregion
 
             public Gregory_Leibniz()
@@ -17,7 +23,7 @@ namespace CalculatePi
             public double Calculate()
             {
                 double result = 0.0;
-                for (int i = 0; i < Iterations; i++)
+                for (int i = StartIteration; i < NumberOfIterations; i++)
                 {
                     result += ((4.0 / (1.0 + i * 4.0)) - (4.0 / (3.0 + i * 4.0)));
                 }
