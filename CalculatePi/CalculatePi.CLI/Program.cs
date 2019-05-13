b7c95cb981b692e23ceb19c7d6f02493d12f9a1b
jonathan_scott <jonathan_scott@icloud.com>
2019-05-13 04:34:16 +0200

    Simple command line utility started. Only supports specifying the number of iterations for one method of estimating Pi.

---
+++ CalculatePi/CalculatePi.CLI/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using CommandLine;
+using CalculatePi.Library;
+
+namespace CalculatePi.CLI
+{
+
+    public class CalculatePi_CLI_Options
+    {
+        [Option('i', "iterations", Required = true)]
+        public int Iterations { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arguments = Parser.Default.ParseArguments<CalculatePi_CLI_Options>(args);
+            arguments.WithParsed<CalculatePi_CLI_Options>(options => Run(options));
+        }
+
+        static void Run(CalculatePi_CLI_Options options)
+        {
+            var pi = new Gregory_Leibniz();
+            pi.NumberOfIterations = options.Iterations;
+            var result = pi.Calculate();
+            Console.WriteLine($"Result = [{result}]");
+        }
+    }
+}
