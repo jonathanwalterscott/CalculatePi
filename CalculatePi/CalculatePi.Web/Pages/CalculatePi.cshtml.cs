ec8cc21b953d0b586c58b7501af79fb1bad896a8
jonathan_scott@icloud.com <jonathan_scott@icloud.com>
2019-05-19 16:28:54 +0900

    Simple Razor page for the user to calculate Pi with the Gregory-Leibniz model with a specified number of iterations.

---
+++ CalculatePi/CalculatePi.Web/Pages/CalculatePi.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using CalculatePi.Library;
+
+namespace CalculatePi.Web.Pages
+{
+    public class CalculatePiModel : PageModel
+    {
+        public CalculatePiModel()
+        {
+            Iterations = 0;
+            Result = "";
+        }
+
+        [BindProperty]
+        public int Iterations { get; set; }
+        [BindProperty]
+        public string Result { get; set; }
+
+        public void OnPost()
+        {
+            var pi = new CalculatePi.Library.Gregory_Leibniz();
+            pi.NumberOfIterations = Iterations;
+            var result = pi.Calculate();
+            Result = $"Result = {result}";
+        }
+    }
+}
