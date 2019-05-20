151399e24381c248238a58c60ed53b39bb84a1c3
jonathan_scott@icloud.com <jonathan_scott@icloud.com>
2019-05-20 14:08:36 +0900

    Allow the user to select the model. Proper input validation has not yet occurred.

--- CalculatePi/CalculatePi.Web/Pages/CalculatePi.cshtml.cs
+++ CalculatePi/CalculatePi.Web/Pages/CalculatePi.cshtml.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using CalculatePi.Library;
+using System.Collections.Generic;
 
 namespace CalculatePi.Web.Pages
 {
@@ -13,13 +15,40 @@ namespace CalculatePi.Web.Pages
         }
 
         [BindProperty]
+        public List<SelectListItem> Models { get; private set; }
+        [BindProperty]
+        public string ModelToUse { get; set; }
+        [BindProperty]
         public int Iterations { get; set; }
         [BindProperty]
         public string Result { get; set; }
 
+        public void OnGet()
+        {
+            Models = new List<SelectListItem> {
+                new SelectListItem { Value = "Gregory-Leibniz", Text = "Gregory-Leibniz"},
+                new SelectListItem { Value = "Nilikantha", Text = "Nilikantha" }
+            };
+        }
+
         public void OnPost()
         {
-            var pi = new CalculatePi.Library.Gregory_Leibniz();
+            Models = new List<SelectListItem> {
+                new SelectListItem { Value = "Gregory-Leibniz", Text = "Gregory-Leibniz"},
+                new SelectListItem { Value = "Nilikantha", Text = "Nilikantha" }
+            };
+            IterativeMethod pi = null;
+            switch (ModelToUse)
+            {
+                case "Gregory-Leibniz" :
+                    pi = new Gregory_Leibniz();
+                    break;
+                case "Nilikantha":
+                    pi = new Nilikantha();
+                    break;
+                default:
+                    throw new System.Exception($"Could not find [{ModelToUse}] model.");
+            }
             pi.NumberOfIterations = Iterations;
             var result = pi.Calculate();
             Result = $"Result = {result}";
