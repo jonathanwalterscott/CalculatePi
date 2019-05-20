4397cb1b055e0e96ea1016d9941e34af64ab1916
Jonathan Scott <jonathan_scott@icloud.com>
2019-05-20 16:55:02 +0900

    Fix server-side validation on the inputs. Works only for English and will look at localization later. Rename page to ..._Html.cshtml so that I can create different frontend implementations, next being a Single-Page Application with a Web API backend.

--- CalculatePi/CalculatePi.Web/Pages/CalculatePi_Html.cshtml.cs
+++ CalculatePi/CalculatePi.Web/Pages/CalculatePi_Html.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using CalculatePi.Library;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace CalculatePi.Web.Pages
 {
@@ -17,26 +18,35 @@ namespace CalculatePi.Web.Pages
         [BindProperty]
         public List<SelectListItem> Models { get; private set; }
         [BindProperty]
+        [Required (ErrorMessage = "Model is required")]
         public string ModelToUse { get; set; }
         [BindProperty]
+        [Required]
+        [Range(0,100000000)]
         public int Iterations { get; set; }
         [BindProperty]
         public string Result { get; set; }
 
-        public void OnGet()
+        public IActionResult OnGet()
         {
             Models = new List<SelectListItem> {
                 new SelectListItem { Value = "Gregory-Leibniz", Text = "Gregory-Leibniz"},
                 new SelectListItem { Value = "Nilikantha", Text = "Nilikantha" }
             };
+            return Page();
         }
 
-        public void OnPost()
+        public IActionResult OnPost()
         {
             Models = new List<SelectListItem> {
                 new SelectListItem { Value = "Gregory-Leibniz", Text = "Gregory-Leibniz"},
                 new SelectListItem { Value = "Nilikantha", Text = "Nilikantha" }
             };
+            if (!ModelState.IsValid)
+            {
+                Result = "";
+                return Page();
+            }
             IterativeMethod pi = null;
             switch (ModelToUse)
             {
@@ -52,6 +62,7 @@ namespace CalculatePi.Web.Pages
             pi.NumberOfIterations = Iterations;
             var result = pi.Calculate();
             Result = $"Result = {result}";
+            return Page();
         }
     }
 }
