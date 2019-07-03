184b3650a4a80b9030b1aeb1aeec62d36cc04190
Jonathan Scott <jonathan_scott@icloud.com>
2019-07-03 14:28:44 +0900

    Remove unnecessary extra configuration. Remove unnecessary using statements.

--- CalculatePi.Web/Program.cs
+++ CalculatePi.Web/Program.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Net;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-using System.Security.Cryptography.X509Certificates;
 
 namespace CalculatePi.Web
 {
@@ -18,14 +14,6 @@ namespace CalculatePi.Web
         public static IWebHost CreateWebHost(string[] args)
         {
             var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-            var config = new ConfigurationBuilder()
-                .SetBasePath("/home/www-user/calculatepi/")
-                .AddJsonFile("appsettings.json", optional: false)
-                .AddJsonFile($"appsettings.{environment}.json", optional: false)
-                .AddEnvironmentVariables()
-                .Build();
-
-            System.Console.WriteLine($"Environment = {environment}");
 
             var webHost = WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
